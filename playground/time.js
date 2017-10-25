408c08e4d9f3950fbf5b9f898cb66341e09b7282
Almeida Jessica <almeida.jessica@outlook.com>
2017-10-25 20:38:03 +0200

    Format timestamps using momentjs

--- playground/time.js
+++ playground/time.js
@@ -3,5 +3,12 @@ const moment = require('moment');
 // var date = new Date();
 // console.log(date.getMonth());
 
-var date = moment();
-console.log(date.format('MMM YYYY'));
+// var date = moment();
+// date.add(1, 'year').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+// 10:35 am 
+// 6.01 am
+
+console.log(moment().format('hh:mm a'));
+console.log(moment().format('h:mm a'));
